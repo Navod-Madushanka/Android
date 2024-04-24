6c3c7d39d5748a63dde9ae789a4e24dafaaa59a3
Navod Madushanka <navodmaithripala@gmail.com>
2024-04-24 13:20:59 +0530

    basic functions

---
+++ LayoutApp/app/src/main/java/com/navod/layoutapp/RelativeLayoutDemo.java
@@ -0,0 +1,14 @@
+package com.navod.layoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativeLayoutDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout_demo);
+    }
+}
